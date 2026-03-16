dcd545dbf4a40b7b5b101f5da498a5bb3f4445d2
tmiraclemabale <49069633+tmiraclemabale@users.noreply.github.com>
2026-03-16 04:55:39 +0200

    Update contact_unlink.php
    Comments

--- pages/contact_unlink.php
+++ pages/contact_unlink.php
@@ -1,21 +1,22 @@
 <?php
     include '../db/binarycity_db.php';
-
+    //Checks if post contains client id to link contact with
     if(isset($_POST['unlink_client_contact'])){
         $client_id = intval($_POST['client_id']);
-
+        //if client id does not exist, returns with message, else procees to execute
         if(!$client_id){
             header("Location: ../?no-client-selected=true");
-        }
-
-        $stmt = $conn->prepare("DELETE FROM contacts WHERE client_id=?");
-        $stmt->bind_param("i",$client_id);
-
-        if($stmt->execute()){
-            header("Location: ../?unlink_status=success");
-        } else {
-            header("Location: ../?unlink_status=error");
-        }
+        }else{
+            //Deletes/Removes client linked with contact from contacts table
+            $stmt = $conn->prepare("DELETE FROM contacts WHERE client_id=?");
+            $stmt->bind_param("i",$client_id);
+    
+            if($stmt->execute()){
+                header("Location: ../?unlink_status=success");
+            } else {
+                header("Location: ../?unlink_status=error");
+            }
+        }        
     } else {
         header("Location: ../?serror=true");
     }
